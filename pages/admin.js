ceb6c775f8ca163c8671f630535a29149145c358
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2022-08-20 14:39:12 +0200

    chore: stopped working

---
+++ pages/admin.js
@@ -0,0 +1,110 @@
+import Head from "next/head";
+import {useState} from "react";
+import './config.js';
+export default function Admin({}) {
+    // React States
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // User Login info
+    const database = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: "user2",
+            password: "pass2"
+        }
+    ];
+
+
+
+
+    // Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (<div className="error">{errorMessages.message}</div>);
+
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+
+        const {target} = event;
+
+        const {app_id, client_id, client_secret, installation_id, uname} = target;
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            console.log(e.target.result);
+            global.config.github.issue_installation_token(e.target.result).then(jwt => {
+                console.log(jwt);
+                sessionStorage.setItem("jwt", JSON.stringify(jwt));
+                global.config.github_jwt = jwt;
+                global.config.github.save_to_local_storage();
+                //go to admin/dashboard
+            });
+        }
+        reader.readAsText(uname.files[0]);
+        global.config.github.installation_id = installation_id.value.trim();
+        global.config.github.client_id = client_id.value.trim();
+        global.config.github.client_secret = client_secret.value.trim();
+        global.config.github.app_id = app_id.value.trim();
+
+    };
+    // JSX code for login form
+    const renderForm = (
+        <center>
+            <div className="form">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-container">
+                        <label>App ID</label>
+                        <input type="text" name="app_id" defaultValue={config.github.app_id}/>
+                        {renderErrorMessage("app_id")}
+                    </div>
+                    <div className="input-container">
+                        <label>Client ID</label>
+                        <input type="text" name="client_id" defaultValue={config.github.client_id}/>
+                        {renderErrorMessage("client_id")}
+                    </div>
+                    <div className="input-container">
+                        <label>Client Secret</label>
+                        <input type="text" name="client_secret" defaultValue={config.github.client_secret}/>
+                        {renderErrorMessage("client_secret")}
+                    </div>
+                    <div className="input-container">
+                        <label>Installation ID</label>
+                        <input type="text" name="installation_id" defaultValue={config.github.installation_id} />
+                        {renderErrorMessage("installation_id")}
+                    </div>
+                    <div className="input-container">
+                        <label>Private Key </label>
+                        <input type="file" name="uname"/>
+                        {renderErrorMessage("uname")}
+                    </div>
+                    <div className="button-container">
+                        <input type="submit"/>
+                    </div>
+                </form>
+            </div>
+        </center>
+    );
+
+
+    return (
+        <div className="app">
+            <Head>
+                <title>Admin</title>
+                <meta name="description" content="Admin page for the club website"/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
+                <meta property="og:title" content="Admin"/>
+                <meta property="og:site_name" content="Club Website"/>
+                <meta property="og:description" content="Admin page for the club website"/>
+                <meta property="og:type" content="profile"/>
+                <meta property="og:image" content="/favicon.ico"/>
+            </Head>
+            <div className="login-form">
+                <div className="title">Sign In</div>
+                {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            </div>
+        </div>
+    );
+}
--- pages/config.js
+++ pages/config.js
@@ -1,16 +1,75 @@
 import clubs from "../data_eskul.json";
+import {createAppAuth} from '@octokit/auth-app';
 
-const config  = {
+/*
+Convert a string into an ArrayBuffer
+from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+*/
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+const config = {
     clubs: clubs,
-    site:{
+    site: {
         title: "ESKUL",
         site_name: "eskul-next",
         description: "Eskul is a platform to connect students with their teachers.",
         favicon: "https://github.com/itclub-one/web-eslul/raw/master/img/logo/smeaa.png"
+    },
+    github: {
+        owner: "itclub-one",
+        repo: "eskul-next",
+        branch_name: "main",
+        email: "",
+        username: "",
+        installation_id: "",
+        client_id: "",
+        app_id: "",
+        client_secret: "",
+        issue_installation_token: async function (private_key) {
+            //https://github.com/octokit/auth-app.js/#installation-authentication
+            const auth = createAppAuth({
+                appId: this.app_id,
+                privateKey: private_key,
+                installationId: this.installation_id,
+                clientId: this.client_id,
+                clientSecret: this.client_secret,
+            });
+            return await auth({type: 'installation'});
+        },
+        save_to_local_storage: function () {
+            if (localStorage !== undefined) {
+                localStorage.setItem("github", JSON.stringify(config.github));
+            }
+        },
+        load_from_local_storage: function () {
+            try {
+                if (!localStorage) return;
+            } catch (e) {
+                return;
+            }
+            const github = localStorage.getItem("github");
+            const parsed = JSON.parse(github);
+            if (github) {
+                Object.assign(config.github, parsed);
+            }
+        }
     }
 }
+console.log(config.github)
+//try load from local storage if empty
+if (config.github.client_id === "") {
+    console.log("loading from local storage");
 
-
+    config.github.load_from_local_storage();
+    console.log(config.github)
+}
 
 module.exports = global.config = config;
 
